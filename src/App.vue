2fd476bb82eed541ad5673badb6ef4f3bd0f640b
Bimal Gharti Magar <bimal.gharti.magar@gmail.com>
2020-04-19 23:30:58 +0545

    Added about page and its functions

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,64 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        COVID 19 Dashboard
-      </div>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">COVID 19 Dashboard</div>
 
       <v-spacer></v-spacer>
 
+      <v-btn text @click="showAbout = true">About</v-btn>
+      <v-menu :open-on-hover="true" :offset-y="true">
+        <template v-slot:activator="{ on }">
+          <v-btn icon dark href v-on="on">
+            <v-img max-width="28" :src="require('./assets/github.png')" />
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item target="_blank" href="https://github.com/bimalghartimagar/covid19-ui">
+            <v-list-item-title>COVID19 - UI</v-list-item-title>
+            <v-list-item-action>
+              <v-btn icon>
+                <v-icon color="blue">mdi-open-in-new</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </v-list-item>
+          <v-list-item target="_blank" href="https://github.com/bimalghartimagar/covid19-api">
+            <v-list-item-title>COVID19 - API</v-list-item-title>
+            <v-list-item-action>
+              <v-btn icon>
+                <v-icon color="blue">mdi-open-in-new</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
 
     <v-content>
-      <Dashboard/>
+      <Dashboard />
     </v-content>
+    <About :dialog="showAbout" v-on:close-about="closeDialog" />
   </v-app>
 </template>
 
 <script>
 import Dashboard from './components/Dashboard'
+import About from './components/About'
 
 export default {
   name: 'App',
 
   components: {
-    Dashboard
+    Dashboard,
+    About
   },
 
   data: () => ({
-    //
-  })
+    showAbout: false
+  }),
+  methods: {
+    closeDialog () {
+      this.showAbout = false
+    }
+  }
 }
 </script>
---
+++ src/components/About.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="text-center">
+    <v-dialog v-model="dialog" width="500">
+      <v-card>
+        <v-card-title class="headline grey lighten-2" primary-title>About Dashboard</v-card-title>
+        <v-list dense flat>
+          <v-subheader>Frontend</v-subheader>
+          <v-list-item-group color="primary">
+            <v-list-item v-for="(item, i) in frontend" :key="i" target="_blank" :href="item.link">
+              <v-list-item-content>
+                <v-list-item-title>
+                  <v-btn small text>{{item.name}}</v-btn>
+                </v-list-item-title>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-btn icon>
+                  <v-icon color="blue">mdi-open-in-new</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list-item-group>
+          <v-divider></v-divider>
+          <v-subheader>API</v-subheader>
+          <v-list-item-group color="primary">
+            <v-list-item v-for="(item, i) in api" :key="i" target="_blank" :href="item.link">
+              <v-list-item-content>
+                <v-list-item-title>
+                  <v-btn small text>{{item.name}}</v-btn>
+                </v-list-item-title>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-btn icon>
+                  <v-icon color="blue">mdi-open-in-new</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list-item-group>
+          <v-divider></v-divider>
+          <v-subheader>Data Source</v-subheader>
+          <v-list-item-group color="primary">
+            <v-list-item v-for="(item, i) in data" :key="i" target="_blank" :href="item.link">
+              <v-list-item-content>
+                <v-list-item-title>
+                  <v-btn small text>{{item.name}}</v-btn>
+                </v-list-item-title>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-btn icon>
+                  <v-icon color="blue">mdi-open-in-new</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="$emit('close-about')">Okay</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['dialog'],
+  data () {
+    return {
+      frontend: [
+        { name: 'Vue', link: 'https://vuejs.org/' },
+        { name: 'Vuetify', link: 'https://vuetifyjs.com/' },
+        { name: 'Chart.js', link: 'https://www.chartjs.org/' },
+        { name: 'vue-chartjs', link: 'https://vue-chartjs.org/' }
+      ],
+      api: [
+        { name: 'Flask', link: 'https://flask.palletsprojects.com/' },
+        { name: 'pandas', link: 'https://pandas.pydata.org/' }
+      ],
+      data: [
+        {
+          name: 'Data on COVID-19',
+          link: 'https://github.com/owid/covid-19-data'
+        }
+      ]
+    }
+  }
+}
+</script>
